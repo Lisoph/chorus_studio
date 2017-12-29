dc3bda2988e5e958c43d3266e39c526a2eac94be
Daniel Hauser <daniel.hauser@liwest.at>
2017-12-29 23:54:06 +0100

    Animate background for no reason; forward window resizing to gl_window

--- src/gui/main_window.rs
+++ src/gui/main_window.rs
@@ -3,15 +3,37 @@ use glutin;
 use glutin::GlContext;
 use nanovg;
 
+use std::time::Instant;
+
 use gui;
 
 const INITIAL_WINDOW_SIZE: (u32, u32) = (1280, 720);
+const RED: gui::Color = gui::Color {
+    r: 0xff as f32 / 255.0,
+    g: 0xca as f32 / 255.0,
+    b: 0x77 as f32 / 255.0,
+    a: 1.0,
+};
+//const BLUE: gui::Color = gui::Color {
+//    r: 0x2e as f32 / 255.0,
+//    g: 0x50 as f32 / 255.0,
+//    b: 0x77 as f32 / 255.0,
+//    a: 1.0,
+//};
+const BLUE: gui::Color = gui::Color {
+    r: 112 as f32 / 255.0,
+    g: 48 as f32 / 255.0,
+    b: 160 as f32 / 255.0,
+    a: 1.0,
+};
+
 
 /// Chorus Studio's main window.
 pub struct MainWindow {
     events_loop: glutin::EventsLoop,
     gl_window: glutin::GlWindow,
     pub was_closed: bool,
+    start_time: Instant,
 }
 
 impl MainWindow {
@@ -37,15 +59,18 @@ impl MainWindow {
             events_loop,
             gl_window,
             was_closed: false,
+            start_time: Instant::now(),
         })
     }
 
     /// Update and draw the window for one frame.
     pub fn update_draw(&mut self, view: &mut gui::View, context: &nanovg::Context) {
         let mut was_closed = self.was_closed;
+        let gl_window = &self.gl_window;
         self.events_loop.poll_events(|event| match event {
            glutin::Event::WindowEvent { event, .. } => match event {
                glutin::WindowEvent::Closed => was_closed = true,
+               glutin::WindowEvent::Resized(w, h) => gl_window.resize(w, h),
                _ => {}
            },
             _ => {}
@@ -54,7 +79,12 @@ impl MainWindow {
 
         let (w, h) = self.gl_window.get_inner_size().unwrap_or(INITIAL_WINDOW_SIZE);
         let (w, h) = (w as i32, h as i32);
+
         unsafe {
+            let elapsed = self.start_time.elapsed();
+            let elapsed = (elapsed.as_secs() as f64 + elapsed.subsec_nanos() as f64 * 1e-9) as f32;
+            let color = nanovg::Color::lerp(RED.into(), BLUE.into(), (elapsed / 2.0).cos() * 0.5 + 0.5);
+            gl::ClearColor(color.red(), color.green(), color.blue(), color.alpha());
             gl::Viewport(0, 0, w, h);
             gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT | gl::STENCIL_BUFFER_BIT);
         }
